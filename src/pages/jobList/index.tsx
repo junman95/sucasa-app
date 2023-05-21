0b7b6ecca1d8ea9e1096bdfe1abc43b97ab018f4
unknown <junman9595@gmail.com>
2023-05-21 16:57:08 +0900

    feat:차크라 ui 테이블 컴포넌트 호출 테ㅡㅌ

---
+++ src/pages/jobList/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Table from "../../components/common/Table";
+
+type Props = {};
+
+const index = (props: Props) => {
+  return (
+    <div>
+      <Table></Table>
+    </div>
+  );
+};
+
+export default index;
