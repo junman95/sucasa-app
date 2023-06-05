6e34dfb3c0c58de8645eafffdb45c149f69191c9
junman <junman_choi@tmax.co.kr>
2023-06-05 19:31:41 +0900

    fix:하이드레이션 에러 조치;

--- src/components/common/TableCard/index.tsx
+++ src/components/common/TableCard/index.tsx
@@ -1,14 +1,17 @@
 import { Card, CardBody, CardHeader, Heading } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const TableCard = ({ children }: Props) => {
-  const [date, setDate] = useState(new Date().toLocaleTimeString());
+  const [date, setDate] = useState("");
+  useEffect(() => {
+    setDate(new Date().toLocaleTimeString());
+  }, []);
   return (
-    <Card w={'50%'} margin={'10px'}>
+    <Card w={"50%"} margin={"10px"}>
       <CardHeader>
         <Heading>금일 {date} 기준 집계된 결과입니다. </Heading>
       </CardHeader>
