9189b539c1b8f8a4b7f46d44216ada45be5a425a
junman <junman_choi@tmax.co.kr>
2023-06-09 18:47:51 +0900

    issue: async await issue

--- src/network/api/eduApi.ts
+++ src/network/api/eduApi.ts
@@ -1,18 +1,17 @@
 import { EduProgram } from "../../features/eduTable/type";
 import { apiClient } from "../apiClient";
 
-const urlPath = process.env.REACT_APP_API_URL;
+const urlPath = process.env.NEXT_PUBLIC_API_URL;
 const dataType = "json";
 const baseUrl = urlPath + "/" + dataType;
 
 export const eduServiceCode = "TBordCont5";
 
-export interface EduResponse {
-  statusCode: number;
-  data: { TBordCont5: { row: EduProgram[] } };
+export interface EduData {
+  row: EduProgram[];
 }
 
-export const requestEduPrograms = async (): Promise<EduResponse> => {
+export const requestEduPrograms = async (): Promise<EduData> => {
   const res = await fetch(`${baseUrl}/${eduServiceCode}/1/100`, {
     next: { revalidate: 10 },
   });
@@ -21,10 +20,5 @@ export const requestEduPrograms = async (): Promise<EduResponse> => {
     throw new Error("error");
   }
 
-  return await res.json();
-  // return await apiClient.get(`/${eduServiceCode}/1/100`);
-  // } catch (error) {
-  //   console.error(error);
-  //   return Error("error");
-  // }
+  return await res.json().then((data) => data[eduServiceCode]);
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,24 +1,19 @@
-import React, { useEffect, useState } from "react";
 import { requestEduPrograms, eduServiceCode } from "../../network/api/eduApi";
 import TableCard from "../../components/common/TableCard";
 import EduTable from "../../features/eduTable";
-import { EduProgram } from "../../features/eduTable/type";
 import store from "../../store";
 import { setPrograms } from "../../features/eduTable/eduSlice";
 
 async function getData() {
   const response = await requestEduPrograms();
-  console.log(response.data);
-  // if (!response.data) {
-  //   throw new Error("데이터를 불러오는데 실패했습니다.");
-  // }
-  store.dispatch(
-    setPrograms(response.data[eduServiceCode].row as EduProgram[])
-  );
+
+  store.dispatch(setPrograms(response.row));
+  return response;
 }
 
-const Home = async () => {
+const home = async () => {
   const data = await getData();
+  console.log(data);
   return (
     <div>
       <TableCard>
@@ -28,4 +23,4 @@ const Home = async () => {
   );
 };
 
-export default Home;
+export default home;
