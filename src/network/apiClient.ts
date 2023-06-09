010cbe347a538cfbe98c50214163e255935bad77
junman <junman_choi@tmax.co.kr>
2023-06-09 10:33:55 +0900

    fix:env

--- src/network/apiClient.ts
+++ src/network/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const urlPath = process.env.REACT_APP_API_URL
+const urlPath = process.env.NEXT_PUBLIC_API_URL
 const dataType = "json";
 export const apiClient = axios.create({
   baseURL: urlPath + '/' + dataType,
